4ee2cdfc1e71b1e63fe4723141f2879d6f4a06c3
David Kurbanov <63120629+TheAzne@users.noreply.github.com>
2022-04-07 14:30:59 +0200

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  const [correctWord, setCorrectWord] = useState;
+
+
+  useEffect(()=>{
+    const loadWord = async()=>{
+      const res = await fetch("/api/word/:length/:allowRepeats");
+      const data = res.json();
+      setCorrectWord(data.word);
+    };
+
+
+    loadWord();
+
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
--- server/src/app.js
+++ server/src/app.js
@@ -3,46 +3,22 @@ import { engine } from "express-handlebars";
 import cors from "cors";
 import { fetchWord } from "./randomWord.js";
 
-
 const app = express();
-app.use(express.static('public', {
-    extensions: ['html']
-}))
-
+app.use(
+  express.static("public", {
+    extensions: ["html"],
+  })
+);
 
 app.use(cors());
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
-
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 app.get("/api/word/:length/:allowRepeats", async (req, res) => {
-    const unique = req.params.length;
-    const repeat = req.params.allowRepeats;
-    const word = await fetchWord(unique, repeat);
-    res.json({ word });
+  const word = await fetchWord(req.params.length, req.params.allowRepeats);
+  res.json({ word });
 });
-
-// app.get('/api/word/', async (req, res) => {
-//     const unique = req.query.unique === 'true';
-//     const wordLength = parseInt(req.query.length);
-//     const word = await fetchRandomWord(wordLength, unique);
-//     res.json({ word });
-//   });
-
-
-// app.get('/api/word/', async (req, res) => {
-//     const unique = req.query.unique === 'true';
-//     const wordLength = parseInt(req.query.length);
-//     const word = await fetchRandomWord(wordLength, unique);
-//     res.json({ word });
-//   });
-
-
-
-
-
-
 
 export default app;
